104220f63f3f6d921fab999669b6ba28f79e5a9b
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-04-26 23:22:43 +0200

    Created form for completing order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,26 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 
 import Modal from "../UI/Modal";
 import CartItem from "./CartItem";
 import CartContext from "../../store/cart-context";
+import Checkout from "./Checkout";
 
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  const [isSubmitted, setIsSubmitted] = useState(false) 
+  const [checkoutForm, setCheckoutForm] = useState(false);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id)
+    cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
-    cartCtx.addItem({...item, amount:1});
+    cartCtx.addItem({ ...item, amount: 1 });
   };
 
-  const cartItems = (
+  const orderHandler = () => {
+    setCheckoutForm(true);
+  };
+
+  const updateIsSubmitted = (value) => {
+    setIsSubmitted(value)
+  }
+
+  useEffect(() => {
+    console.log(isSubmitted)
+    if(isSubmitted){
+      cartCtx.clearCart()
+      setIsSubmitted(false)
+    }
+  },[cartCtx, isSubmitted])
+  
+  let cartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
         <CartItem
@@ -28,11 +47,24 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null,item.id)}
-          onAdd={cartItemAddHandler.bind(null,item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
         />
       ))}
     </ul>
+  );
+
+  const modalActions = (
+    <div className={classes.actions}>
+      <button className={classes["button--alt"]} onClick={props.onHideCart}>
+        Close
+      </button>
+      {hasItems && (
+        <button className={classes.button} onClick={orderHandler}>
+          Pay
+        </button>
+      )}
+    </div>
   );
 
   return (
@@ -42,12 +74,8 @@ const Cart = (props) => {
         <span>Total amount</span>
         <span>{totalAmount}</span>
       </div>
-      <div className={classes.actions}>
-        <button className={classes["button--alt"]} onClick={props.onHideCart}>
-          Close
-        </button>
-        {hasItems && <button className={classes.button}>Pay</button>}
-      </div>
+      {checkoutForm && <Checkout onCancel = {props.onHideCart} setIsSubmitted = {updateIsSubmitted}/>}
+      {!checkoutForm && modalActions}
     </Modal>
   );
 };
