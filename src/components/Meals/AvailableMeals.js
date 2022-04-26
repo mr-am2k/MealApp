511923f99c44813223efdd582192d1479ea416e8
Muamer Aličković <alickovicmuamer1@gmail.com>
2022-04-26 18:03:38 +0200

    Added file fore meal data, and replaced firebase with it

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -4,53 +4,9 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
+import meals from "../../data/data.json"
 
 const AvailableMeals = () => {
-  const [meals, setMeals] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [httpError, setHttpError] = useState();
-
-  useEffect(() => {
-    const fetchMeals = async () => {
-      const response = await fetch(
-        "https://meals-2ee23-default-rtdb.europe-west1.firebasedatabase.app/meals.jso"
-      );
-      if (!response.ok) {
-        throw new Error("Something went wrong");
-      }
-      const data = await response.json();
-      const loadedMeals = [];
-      for (const key in data) {
-        loadedMeals.push({
-          id: key,
-          name: data[key].name,
-          description: data[key].description,
-          price: data[key].price,
-        });
-      }
-      setMeals(loadedMeals);
-      setIsLoading(false);
-    };
-    fetchMeals().catch((error) => {
-      setIsLoading(false);
-      setHttpError(error.message);
-    });
-  }, []);
-
-  if (isLoading) {
-    return (
-      <section className={classes.MealsLoading}>
-        <p>Loading...</p>
-      </section>
-    );
-  }
-  if (httpError) {
-    return (
-      <section className={classes.MealsError}>
-        <p>{httpError}</p>
-      </section>
-    );
-  }
   const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
